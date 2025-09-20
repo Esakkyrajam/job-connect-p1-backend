23d985f710989f29832eac9f0fbd54a864ee8a0d
Esakky Raja M <113232460+Esakkyrajam@users.noreply.github.com>
2025-09-20 21:40:04 +0530

    Update SecurityConfig.java

--- src/main/java/com/jobconnect/config/SecurityConfig.java
+++ src/main/java/com/jobconnect/config/SecurityConfig.java
@@ -48,18 +48,6 @@ public class SecurityConfig {
     }
 
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:5173")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
 
 
     @Bean
